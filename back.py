791b3e8d95665c55d889e0cb9b7ec2bb5a12f074
Sina Jafari <168327662+sina0101@users.noreply.github.com>
2025-02-05 02:15:04 +0330

    Update back.py

--- back.py
+++ back.py
@@ -1,144 +1,94 @@
 import sqlite3
 
+class BookBank:
+    def __init__(self, db_name='database.db'):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
+        self.create_table()
 
-global cur
-db=sqlite3.connect('database.db')
-cur=db.cursor()
+    def create_table(self):
+        """Create the book_bank table if it doesn't exist."""
+        self.cur.execute('''
+            CREATE TABLE IF NOT EXISTS book_bank(
+                id INTEGER PRIMARY KEY,
+                title VARCHAR(30),
+                author VARCHAR(30),
+                year INTEGER,
+                subject VARCHAR(20)
+            )
+        ''')
+        self.conn.commit()
 
+    def insert(self, title, author, year, subject):
+        """Insert a new book into the book_bank table."""
+        self.cur.execute('''
+            INSERT INTO book_bank(title, author, year, subject) 
+            VALUES(?, ?, ?, ?)
+        ''', (title, author, year, subject))
+        self.conn.commit()
 
+    def edit(self, id, field, new_value):
+        """Edit a book's information based on the given field."""
+        if field in ['title', 'author', 'year', 'subject']:
+            self.cur.execute(f'''
+                UPDATE book_bank 
+                SET {field} = ? 
+                WHERE id = ?
+            ''', (new_value, id))
+            self.conn.commit()
+        else:
+            raise ValueError("Invalid field specified")
 
+    def delete_with_id(self, id_for_delete):
+        """Delete a book from the book_bank table based on its ID."""
+        self.cur.execute('''
+            DELETE FROM book_bank 
+            WHERE id = ?
+        ''', (id_for_delete,))
+        self.conn.commit()
 
+    def search(self, field, search_value):
+        """Search for books based on a specific field and value."""
+        if field in ['title', 'author', 'year', 'subject']:
+            self.cur.execute(f'''
+                SELECT * 
+                FROM book_bank 
+                WHERE {field} = ?
+            ''', (search_value,))
+            return self.cur.fetchall()
+        else:
+            raise ValueError("Invalid field specified")
 
-def create():
-    
-    cur.execute('''
-          CREATE TABLE IF NOT EXISTS book_bank(
-                
-            id INTEGER PRIMARY KEY,
-            title VARCHAR(30),
-            author VARCHAR(30),
-            year INTEGER,
-            subject VARCHAR(20)
-          )      
-                
-                ''')
+    def show_all(self):
+        """Retrieve all books from the book_bank table."""
+        self.cur.execute('SELECT * FROM book_bank')
+        return self.cur.fetchall()
 
-create()
+    def close(self):
+        """Close the database connection."""
+        self.conn.close()
 
-def insert(title,author,year,subject):
-    
-    
-   
-    
-    cur.execute('''
-INSERT INTO book_bank(title,author,year,subject) VALUES(?,?,?,?)
+# Example usage:
+if __name__ == "__main__":
+    db = BookBank()
 
+    # Insert a new book
+    db.insert("The Great Gatsby", "F. Scott Fitzgerald", 1925, "Fiction")
 
-''',(title,author,year,subject))
-    db.commit()
+    # Edit a book's year
+    db.edit(1, 'year', 1926)
 
+    # Search for books by author
+    books_by_author = db.search('author', 'F. Scott Fitzgerald')
+    print(books_by_author)
 
-def edit(id,sell,new_value):
-    if sell=='year':
-    
-    
-        cur.execute('''
-        UPDATE book_bank SET year =?   WHERE id=? 
+    # Show all books
+    all_books = db.show_all()
+    print(all_books)
 
+    # Delete a book by ID
+    db.delete_with_id(1)
 
-    ''',(new_value,id))
-        
-
-    if sell=='author':
-    
-    
-        cur.execute('''
-        UPDATE book_bank SET author =?   WHERE id=? 
-
-
-    ''',(new_value,id))
-
-
-    if sell=='subject':
-        cur.execute('''
-        UPDATE book_bank SET subject =?   WHERE id=? 
-
-
-    ''',(new_value,id))
-
-
-    if sell=='title':
-
-        cur.execute('''
-
-        UPDATE book_bank SET title=? WHERE id=?
-
-''',(new_value,id))
-        
-    db.commit()
-
-
-def delete_with_id(id_for_delete):
-    
-    cur.execute('''
-    DELETE FROM book_bank WHERE id=?
-
-''',(id_for_delete,))
-    db.commit()
-    
-    '''def show_all():
-    cur.execute('SELECT * from book_bank')
-    rows = cur.fetchall()
-    return rows'''
-
-def def_search(v,search_value):
-    
-    if v=='title':
-        res=cur.execute('''
-
-        SELECT * FROM book_bank WHERE title=? 
-
-
-    ''',(search_value,))
-        
-
-    if v=='author':
-        res=cur.execute('''
-
-        SELECT * FROM book_bank WHERE author=? 
-
-
-    ''',(search_value,))
-    
-
-    if v=='year':
-        res=cur.execute('''
-
-        SELECT * FROM book_bank WHERE year=? 
-
-
-    ''',(search_value,))
-        
-    if v=='subject':
-        res=cur.execute('''
-
-        SELECT * FROM book_bank WHERE subject=? 
-
-
-    ''',(search_value,))
-        
-
-
-
-    res=cur.fetchall()
-    return res
-    
-
-
-def show_all():
-    cur.execute('SELECT * from book_bank')
-    rows = cur.fetchall()
-    return rows
-
-    
-
+    # Close the database connection
+    db.close()
